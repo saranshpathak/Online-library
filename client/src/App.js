0db27dbc3b71a2e0c226765caf4e280430024881
Saransh <saransh01pathak@gmail.com>
2022-05-14 17:17:22 +0530

    Set private route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
+  Navigate
 } from "react-router-dom";
 
 import LandingPage from './component/LandingPage'
@@ -12,19 +13,26 @@ import DashBoard from './component/DashBoard';
 import AddBook from './component/AddBook';
 import Edit from './component/Edit';
 
+import { selectUser } from './features/userSlice';
+import { useSelector } from 'react-redux';
 import './App.css'
 
 export default function App() {
+   const user = useSelector(selectUser);
+   function PrivateRoute({ children }) {
+  
+    return user ? children : <Navigate to="/login" />;
+  }
     return (
         <Router>
             <div>
             <Routes>
           <Route exact path="/" element={<LandingPage/>} />
           <Route exact path="/login" element={<LoginPage/>} />
-          <Route exact path="/register" element={<RegisterPage/>} />
-          <Route exact path="/dashboard" element={<DashBoard/>} />
-          <Route exact path="/add" element={<AddBook/>} />
-          <Route exact path="/edit" element={<Edit/>} />
+          <Route exact path="/register" element={<PrivateRoute><RegisterPage/></PrivateRoute>} />
+          <Route exact path="/dashboard" element={<PrivateRoute><DashBoard/></PrivateRoute>} />
+          <Route exact path="/add" element={<PrivateRoute><AddBook/></PrivateRoute>} />
+          <Route exact path="/edit" element={<PrivateRoute><Edit/></PrivateRoute> }/>
         </Routes>
                 
             </div>
